3dc795d0aeded07c87453b89aaed1033aea57602
Dariaengin <dariaengin@gmail.com>
2025-02-26 11:10:49 +0200

    add array exercies

---
+++ Algorithms/Monday/Wednesday/index.js
@@ -0,0 +1,2 @@
+//Declare a function factorialize(n) that takes an integer as an input and returns the factorial of the provided integer.
+
--- JS Advanced/JS Advanced/Array Exercise/index.js
+++ JS Advanced/JS Advanced/Array Exercise/index.js
@@ -1,10 +1,32 @@
-function filterRange(arr, a, b) {
-    return arr.filter(element => element >= a && element <= b);
+// function filterRange(arr, a, b) {
+//     return arr.filter(number => number >= a && number <= b);
+// }
+
+// let arr = [5, 3, 8, 1];
+
+// let filtered = filterRange(arr, 1, 4);
+
+// alert(filtered); 
+// alert(arr); 
+   
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let users = [john, pete, mary];
+
+// let names = users.map(user => user.name);
+
+// console.log(names);
+
+function getAverageAge(users) {
+    return users.reduce((sum, user) => sum + user.age, 0) / users.length;
 }
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
 
-let arr = [5, 3, 8, 1];
+let arr = [john, pete, mary];
 
-let filtered = filterRange(arr, 1, 4);
+console.log(getAverageAge(arr));
 
-alert(filtered);
-alert(arr);  
