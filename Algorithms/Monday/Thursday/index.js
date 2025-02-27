b88c8e4d5f8ee9e22a53bb740cd12470f742ca04
Dariaengin <dariaengin@gmail.com>
2025-02-27 12:28:41 +0200

    change string

--- Algorithms/Monday/Thursday/index.js
+++ Algorithms/Monday/Thursday/index.js
@@ -6,21 +6,21 @@
 // if they are equal return true , otherwise false;
 
 function palindrome(string) {
-    let st = string.split('').join('').toLowerCase()
-    for (let i = 0; i < st.length/2; i++) {
-        if (st[i]!= st[st.length - 1 - i]) { return false }
+    let st = string.split(' ').join('').toLowerCase()//split transforms string into array, then join transform the array into string
+    for (let i = 0; i < st.length/2; i++) {//compare half of the string
+        if (st[i]!= st[st.length - 1 - i]) { return false }//index length - i - 1 (from the end).
     }
     return true
 }
 console.log(palindrome('helleh'))
 
 function palindromChecker(str) {
-    const checkString =str.split(" ").join("").toLowerCase();
+    const string =str.split(" ").join("").toLowerCase();
     let reversedString = "";
-    for (let i = checkString.length - 1; i >= 0; i--){
-        reversedString += checkString[i];
+    for (let i = string.length - 1; i >= 0; i--){//Loops backwards through string
+        reversedString += string[i];
     }
-    return reversedString === checkString;
+    return reversedString === string;
 }
 
 
