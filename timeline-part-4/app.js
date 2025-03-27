339501a6f38e04accc2237082fda1c78269b6b95
Dariaengin <dariaengin@gmail.com>
2025-03-27 16:23:11 +0100

    timeline-part-four

---
+++ timeline-part-4/app.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const routes = require('./config/routes');
+
+require('./config/mongoose');
+
+const app = express();
+const port = 3300;
+
+app.use('/public', express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+app.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
