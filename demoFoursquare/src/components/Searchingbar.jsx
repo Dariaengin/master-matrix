a97336084c2b4a8f0319926e8c08b8636633b9ed
Dariaengin <dariaengin@gmail.com>
2025-04-21 13:56:00 +0200

    add searchingbar

---
+++ demoFoursquare/src/components/Searchingbar.jsx
@@ -0,0 +1,36 @@
+
+import React, { useState } from 'react';
+
+const Searchingbar = ({ onSearch }) => {
+  const [userInput, setUserInput] = useState('');
+
+  // Handle form input change
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log('Search term: ', userInput);
+    onSearch(userInput);
+    setUserInput('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className='searchingbar'>
+        <input
+          type='text'
+          name='user-search'
+          value={userInput}
+          onChange={handleChange}
+          placeholder='input your search request...'
+          required
+        />
+        <button type='submit'>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default Searchingbar;
---
+++ demoFoursquare/src/components/pages/homepage.jsx
@@ -0,0 +1,37 @@
+
+import React, { useState } from 'react';
+import useFetch from '../hooks/useFetch';
+
+import Searchingbar from '../Searchingbar';
+
+const HomePage = () => {
+  const [albums] = useFetch('https://jsonplaceholder.typicode.com/albums');
+  const [filteredAlbums, setFilteredAlbums] = useState([]);
+
+  const handleUserSearch = (userInput) => {
+    const trimmedInput = userInput.trim();
+
+    if (trimmedInput) {
+      const filtered = albums.filter((album) =>
+        album.title.toLowerCase().includes(trimmedInput.toLowerCase())
+      );
+      setFilteredAlbums(filtered);
+    } else {
+      setFilteredAlbums([]);
+    }
+  };
+
+  return (
+    <div className='display-slyles'>
+      <Searchingbar onSearch={handleUserSearch} />
+      {filteredAlbums.length ? <h2>Search results</h2> : null}
+      <ul>
+        {filteredAlbums?.map((album) => (
+          <li key={album.id}>{album.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
